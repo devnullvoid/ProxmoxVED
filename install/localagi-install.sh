c37926116893660bd89e7a6da67441b7413fab1c
John Doe <johndoe@example.com>
2026-03-04 10:42:01 -0500

    fix: update author information in localagi.sh and localagi-install.sh

--- install/localagi-install.sh
+++ install/localagi-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2026 community-scripts ORG
-# Author: GitHub Copilot
+# Author: BillyOutlast
 # License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
 # Source: https://github.com/mudler/LocalAGI
 
