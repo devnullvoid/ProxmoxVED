975f5bcbbe6ced8f3c28a474201ab07743dbd351
John Doe <johndoe@example.com>
2026-03-04 12:27:25 -0500

    fix: remove dynamic COMMUNITY_SCRIPTS_URL assignment in localagi.sh

--- ct/localagi.sh
+++ ct/localagi.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-COMMUNITY_SCRIPTS_URL="${COMMUNITY_SCRIPTS_URL:-${COMMUNITY_SCRIPT_URL:-https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main}}"
-source <(curl -fsSL "$COMMUNITY_SCRIPTS_URL/misc/build.func")
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
 
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: BillyOutlast
