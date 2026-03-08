42ee142ebf761c7aecacef118828a426b4a1e08d
Stephen Chin <steveonjava@gmail.com>
2026-03-08 14:42:19 -0700

    Apply suggestion from @CrazyWolf13
    Co-authored-by: Tobias <96661824+CrazyWolf13@users.noreply.github.com>

--- install/protonmail-bridge-install.sh
+++ install/protonmail-bridge-install.sh
@@ -22,9 +22,7 @@ useradd -r -m -d /home/protonbridge -s /usr/sbin/nologin protonbridge
 install -d -m 0750 -o protonbridge -g protonbridge /home/protonbridge
 msg_ok "Created Service User"
 
-msg_info "Installing Proton Mail Bridge"
 fetch_and_deploy_gh_release "protonmail-bridge" "ProtonMail/proton-bridge" "binary"
-msg_ok "Installed Proton Mail Bridge"
 
 msg_info "Creating Services"
 
