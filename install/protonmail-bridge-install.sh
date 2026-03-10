c65429f2f8708a2218eb15df41ba9e307c647fbc
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2026-03-10 10:06:10 +0100

    fix: correcting file write

--- install/protonmail-bridge-install.sh
+++ install/protonmail-bridge-install.sh
@@ -25,7 +25,7 @@ msg_ok "Created Service User"
 fetch_and_deploy_gh_release "protonmail-bridge" "ProtonMail/proton-bridge" "binary"
 
 msg_info "Creating Services"
-cat <<EOF> /etc/systemd/system/protonmail-bridge.service
+cat <<EOF >/etc/systemd/system/protonmail-bridge.service
 [Unit]
 Description=Proton Mail Bridge (noninteractive)
 After=network-online.target
