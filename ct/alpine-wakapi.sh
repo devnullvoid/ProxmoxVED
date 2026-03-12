fd2162d52755f16e96a2548c632d009b0af86caf
tremor021 <arezina.slavisa@gmail.com>
2026-03-12 14:30:10 +0100

    Wakapi updates

--- ct/alpine-wakapi.sh
+++ ct/alpine-wakapi.sh
@@ -7,9 +7,9 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 # Source: https://wakapi.dev/ | https://github.com/muety/wakapi
 
 APP="Alpine-Wakapi"
-var_tags="${var_tags:-productivity}"
-var_cpu="${var_cpu:-2}"
-var_ram="${var_ram:-1024}"
+var_tags="${var_tags:-code;time-tracking}"
+var_cpu="${var_cpu:-1}"
+var_ram="${var_ram:-512}"
 var_disk="${var_disk:-4}"
 var_os="${var_os:-alpine}"
 var_version="${var_version:-3.23}"
