ec12d0f31a09025c7ce95f09b5c0dd6d70ec96db
tremor021 <arezina.slavisa@gmail.com>
2026-03-18 13:39:51 +0100

    add Teleport script

---
+++ install/teleport-install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2026 community-scripts ORG
+# Author: Slaviša Arežina (tremor021)
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# Source: https://goteleport.com/
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+setup_deb822_repo \
+  "teleport" \
+  "https://deb.releases.teleport.dev/teleport-pubkey.asc" \
+  "https://apt.releases.teleport.dev/debian" \
+  "trixie" \
+  "stable/v18"
+
+msg_info "Configuring Teleport"
+$STD teleport configure -o /etc/teleport.yaml
+systemctl enable -q --now teleport
+$STD tctl users add teleport-admin --roles=editor,access --logins=root >~/teleportadmin.creds
+msg_ok "Configured CaddyManager"
+
+motd_ssh
+customize
+cleanup_lxc
